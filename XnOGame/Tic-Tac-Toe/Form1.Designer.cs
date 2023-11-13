e21bcb0a6358fc8fe7b7a00778805828208897e8
IsraelBola <102138636+IsraelBola@users.noreply.github.com>
2023-11-13 16:27:31 +0200

    Add files via upload

---
+++ XnOGame/Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.winStreakLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputPlayerX = new System.Windows.Forms.TextBox();
+            this.InputPlayerO = new System.Windows.Forms.TextBox();
+            this.PlayerX = new System.Windows.Forms.Label();
+            this.PlayerO = new System.Windows.Forms.Label();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.DimGray;
+            this.A1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.A1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A1.Location = new System.Drawing.Point(12, 42);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(75, 75);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.DimGray;
+            this.B2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B2.Location = new System.Drawing.Point(93, 123);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(75, 75);
+            this.B2.TabIndex = 1;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.DimGray;
+            this.B3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B3.Location = new System.Drawing.Point(174, 123);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(75, 75);
+            this.B3.TabIndex = 2;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.DimGray;
+            this.C3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C3.Location = new System.Drawing.Point(174, 204);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(75, 75);
+            this.C3.TabIndex = 3;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.DimGray;
+            this.C1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C1.Location = new System.Drawing.Point(12, 204);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(75, 75);
+            this.C1.TabIndex = 4;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.DimGray;
+            this.B1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B1.Location = new System.Drawing.Point(12, 123);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(75, 75);
+            this.B1.TabIndex = 5;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.DimGray;
+            this.A3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A3.Location = new System.Drawing.Point(174, 42);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(75, 75);
+            this.A3.TabIndex = 6;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.DimGray;
+            this.A2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A2.Location = new System.Drawing.Point(93, 42);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(75, 75);
+            this.A2.TabIndex = 7;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.DimGray;
+            this.C2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C2.Location = new System.Drawing.Point(93, 204);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(75, 75);
+            this.C2.TabIndex = 9;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // winStreakLabel
+            // 
+            this.winStreakLabel.AutoSize = true;
+            this.winStreakLabel.Location = new System.Drawing.Point(12, 306);
+            this.winStreakLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.winStreakLabel.Name = "winStreakLabel";
+            this.winStreakLabel.Size = new System.Drawing.Size(192, 13);
+            this.winStreakLabel.TabIndex = 11;
+            this.winStreakLabel.Text = "X is on a win streak of 50, click to reset";
+            this.winStreakLabel.Click += new System.EventHandler(this.winStreakLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Input name for \"X\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(410, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Input name for O";
+            // 
+            // InputPlayerX
+            // 
+            this.InputPlayerX.Location = new System.Drawing.Point(276, 70);
+            this.InputPlayerX.Name = "InputPlayerX";
+            this.InputPlayerX.Size = new System.Drawing.Size(100, 20);
+            this.InputPlayerX.TabIndex = 14;
+            this.InputPlayerX.TextChanged += new System.EventHandler(this.InputPlayerX_TextChanged);
+            // 
+            // InputPlayerO
+            // 
+            this.InputPlayerO.Location = new System.Drawing.Point(396, 70);
+            this.InputPlayerO.Name = "InputPlayerO";
+            this.InputPlayerO.Size = new System.Drawing.Size(100, 20);
+            this.InputPlayerO.TabIndex = 15;
+            this.InputPlayerO.TextChanged += new System.EventHandler(this.InputPLayerO_TextChanged);
+            // 
+            // PlayerX
+            // 
+            this.PlayerX.AutoSize = true;
+            this.PlayerX.Location = new System.Drawing.Point(32, 13);
+            this.PlayerX.Name = "PlayerX";
+            this.PlayerX.Size = new System.Drawing.Size(35, 13);
+            this.PlayerX.TabIndex = 16;
+            this.PlayerX.Text = "label3";
+            this.PlayerX.Click += new System.EventHandler(this.PlayerX_Click);
+            // 
+            // PlayerO
+            // 
+            this.PlayerO.AutoSize = true;
+            this.PlayerO.Location = new System.Drawing.Point(165, 13);
+            this.PlayerO.Name = "PlayerO";
+            this.PlayerO.Size = new System.Drawing.Size(35, 13);
+            this.PlayerO.TabIndex = 17;
+            this.PlayerO.Text = "label4";
+            // 
+            // okBtn
+            // 
+            this.okBtn.BackColor = System.Drawing.Color.Silver;
+            this.okBtn.Location = new System.Drawing.Point(347, 105);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.TabIndex = 18;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = false;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(276, 183);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(238, 95);
+            this.listBox.TabIndex = 19;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "SCOREBOARD";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(545, 368);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.PlayerO);
+            this.Controls.Add(this.PlayerX);
+            this.Controls.Add(this.InputPlayerO);
+            this.Controls.Add(this.InputPlayerX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.winStreakLabel);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Label winStreakLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputPlayerX;
+        private System.Windows.Forms.TextBox InputPlayerO;
+        private System.Windows.Forms.Label PlayerX;
+        private System.Windows.Forms.Label PlayerO;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ XnOGame/Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        bool playerTurn = true;// X Turn -> true , O Turn -> false
+        int turnCount = 0;
+        int winStreak = 0; // Positive for X, Negative for O
+        string playerNameX = "";
+        string playerNameO = "";
+
+        
+        public Form1()
+        {
+            Home startPage = new Home();
+            startPage.ShowDialog();
+            InitializeComponent();
+            updateWinStreak("");
+            // Load existing winners from the text file
+            DisplayWinner();
+           
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button theButtton = (Button)sender;
+
+            if (playerTurn)
+            {
+                theButtton.Text = "X";
+                theButtton.Enabled = false;
+            }
+            else
+            {
+                theButtton.Text = "O";
+                theButtton.Enabled = false;
+            }
+
+            turnCount++;//for the draws, the maxium play is 9
+            playerTurn = !playerTurn;
+            checkWinner();
+        }
+
+        private void checkWinner()
+        {
+            /*
+            foreach(Control x in Controls)//emunmeration of all controls{}
+            */
+            bool weHaveWinner = false;
+
+            //Switch possible ?
+
+            //---
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A2.Enabled))
+                weHaveWinner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C2.Enabled))
+                weHaveWinner = true;
+
+            // |||
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!B1.Enabled))
+                weHaveWinner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!B3.Enabled))
+                weHaveWinner = true;
+
+            //X
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+
+            
+
+
+            if (weHaveWinner)
+            {
+                //disableAllbtn();
+
+                String winner = "";
+
+                if (playerTurn)
+                    winner = PlayerO.Text;
+                else
+                    winner = PlayerX.Text;
+                updateWinStreak(winner);
+                LoadWinner(winner);
+
+                MessageBox.Show(winner + " Wins!", "GG");
+                autoNewGame();
+                DisplayWinner();
+
+            }
+            else
+            {
+                if(turnCount == 9)
+                {
+                    MessageBox.Show("Draw");
+            
+                    //disableAllbtn();
+                    autoNewGame();
+                }
+
+            }
+
+        }
+
+        /*
+        private void disableAllbtn()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }//Ignoring the menu strip (is not a button)
+        }*/
+
+        private void autoNewGame()
+        {
+            playerTurn = true;
+            turnCount = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    // This check is important. You can not rely on the exception handling to only process buttons.
+                    // If another type of control is encountered, the exception handling will cause this code to exit
+                    // without necessarily clearing all the buttons.
+                    if (c is Button)
+                    {
+                        (c as Button).Enabled = true;
+                        (c as Button).Text = "";
+                    }
+                }
+            }
+            catch { }
+        } 
+
+        /// <summary>
+        /// Updates the label indicating the current win streak
+        /// </summary>
+        /// <param name="winner">Can take "X" or "O" or any other value to reset</param>
+        private void updateWinStreak(string winner)
+        {
+            if (winner == "X")
+            {
+                if (winStreak < 0) // If O was on a win streak, zero it before incrementing
+                    winStreak = 0;
+                winStreak++;
+            }
+            else if (winner == "O")
+            {
+                if (winStreak > 0) // If X was on a win streak, zero it before decrementing
+                    winStreak = 0;
+                winStreak--;
+            }
+            else
+            {
+                winStreak = 0;
+            }
+
+            winStreakLabel.Visible = (winStreak != 0);
+            winStreakLabel.Text = String.Format("{0} is on a win streak of {1}", winner, Math.Abs(winStreak));
+        }
+
+        private void winStreakLabel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to reset the win streak?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                updateWinStreak("");
+            }
+        }
+
+        private void InputPlayerX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputPLayerO_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayerX_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            //Get the names added in the textbox
+            playerNameX = InputPlayerX.Text;
+            playerNameO = InputPlayerO.Text;
+
+            //assign the names in the labels
+            PlayerX.Text = $"{playerNameX} - X";
+            PlayerO.Text = $"{playerNameO} - O";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=Israel-Lenovo;Initial Catalog=TickTackToe;Integrated Security=True";
+           SqlConnection connection = new SqlConnection(connectionString);
+           
+            connection.Open();
+
+            string query = "DELETE FROM Winner;";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.ExecuteNonQuery();  
+            }
+
+
+                // Clear the ListBox
+                listBox.Items.Clear();
+            MessageBox.Show("Scoreboard has been cleared.");
+
+            connection.Close();
+
+            
+        }
+
+        public void LoadWinner(string name)
+        {
+            //specify address of server and database
+            string scoreboardFilePath = "Data Source=Israel-Lenovo;Initial Catalog=TickTackToe;Integrated Security=True";
+            //Establish connection
+            using (SqlConnection sqlCon = new SqlConnection(scoreboardFilePath))
+            {
+                //Open connection
+                sqlCon.Open();
+
+                string query = "INSERT INTO Winner (Firstname) VALUES('" + name + "');";
+
+                //executing the query
+                using (SqlCommand cmd = new SqlCommand(query, sqlCon))
+                {
+                    // Use parameters to prevent SQL injection
+                    cmd.Parameters.AddWithValue("Firstname", name);   
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Person has been saved to database table!");
+                sqlCon.Close();
+            }
+
+        }
+
+        private void DisplayWinner()
+        {
+            string connectionString = "Data Source=Israel-Lenovo;Initial Catalog=TickTackToe;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT Firstname FROM Winner;"; 
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    listBox.Items.Clear(); // Clear the ListBox first
+
+                    while (reader.Read())
+                    {
+                        string name = reader["Firstname"].ToString();
+                        listBox.Items.Add(name);// Add the names to the ListBox
+                    }
+                }
+                connection.Close();
+            }
+
+        }
+
+    }
+}
